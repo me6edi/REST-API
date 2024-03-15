c95e8699fe114f80bd013047eb44a3c1d1ad3c16
Mehedi Amin <exited17@gmail.com>
2024-03-15 20:37:36 +0600

    Custompermission

--- Session_Authentication/api/views.py
+++ Session_Authentication/api/views.py
@@ -2,8 +2,8 @@ from .models import Studnet
 from .serializers import StudentSerializer
 from rest_framework import viewsets
 from rest_framework.authentication import SessionAuthentication
-from rest_framework.permissions import IsAuthenticated,AllowAny,IsAdminUser,IsAuthenticatedOrReadOnly
-
+# from rest_framework.permissions import IsAuthenticated,AllowAny,IsAdminUser,IsAuthenticatedOrReadOnly,DjangoModelPermissions,DjangoModelPermissionsOrAnonReadOnly
+from .Custompermission import Mypermission 
 # Model View Set 
 class StudentModelViewSet(viewsets.ModelViewSet):
     queryset = Studnet.objects.all()
@@ -12,5 +12,14 @@ class StudentModelViewSet(viewsets.ModelViewSet):
     # permission_classes = [IsAuthenticated]
     # permission_classes  = [AllowAny]
     # permission_classes  = [IsAdminUser]
-    permission_classes  = [IsAuthenticatedOrReadOnly]
+    # permission_classes  = [IsAuthenticatedOrReadOnly]
+    # permission_classes  = [DjangoModelPermissions]
+    # permission_classes  = [DjangoModelPermissionsOrAnonReadOnly]
+
+
+
+
+# Custom Permission 
+    permission_classes = [Mypermission]
+
 
