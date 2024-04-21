3165c0c4516ad8976e03c7961abb7dfb5ec675b4
Mehedi Amin <exited17@gmail.com>
2024-04-21 22:03:26 +0600

    Generate Token

--- Token_Authentication/Token_Authentication/urls.py
+++ Token_Authentication/Token_Authentication/urls.py
@@ -15,8 +15,20 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from api import views
+from rest_framework.routers import DefaultRouter
+# from rest_framework.authtoken.views import obtain_auth_token
+from api.auth import CustomAuthToken
+# Creating Router Object
+router = DefaultRouter()
+
+# Register StudnetViewSet With Router
+router.register('studentapi', views.StudentModelViewSet, basename = 'Student')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(router.urls)),
+    path('auth/',include('rest_framework.urls', namespace='rest_framework')),
+    path('gettoken/', CustomAuthToken.as_view())
 ]
--- Token_Authentication/api/models.py
+++ Token_Authentication/api/models.py
@@ -5,4 +5,14 @@ class Studnet(models.Model):
     roll = models.IntegerField()
     city = models.CharField(max_length=50)
 
-    
+
+from django.conf import settings
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from rest_framework.authtoken.models import Token
+
+# This singnal creates Auth Token for users
+@receiver(post_save, sender=settings.AUTH_USER_MODEl)
+def create_auth_token(sender, instance=None, Created=False, **kwargs):
+    if Created:
+        Token.objects.create(user=instance)
