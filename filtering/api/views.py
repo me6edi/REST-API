9333c41b7b743e93e42dedd0b619df2eaf88f2ca
me6edi <exited17@gmail.com>
2024-07-07 21:35:04 +0600

    ordering and search filter

--- filtering/api/views.py
+++ filtering/api/views.py
@@ -2,11 +2,36 @@ from django.shortcuts import render
 from .models import Student
 from .serializers import StudentSerializer
 from rest_framework.generics import ListAPIView
+from rest_framework.filters import SearchFilter
+from rest_framework.filters import OrderingFilter
 
 # Create your views here.
 
+# Filtering in django
+
+# class StudentList(ListAPIView):
+#     queryset = Student.objects.all()
+#     serializer_class = StudentSerializer
+#     # filter in django
+#     # filters_fields = ['city']
+#     # filters_fields = ['name','city']
+
+
+# Search Filter in Django
+
+# class StudentList(ListAPIView):
+#     queryset = Student.objects.all()
+#     serializer_class = StudentSerializer
+#     # Search filter
+#     filter_backends = [SearchFilter]
+#     # search_fields = ['name','city']
+#     # search_fields = ['^name'] # '^' Starts with search any character.
+
+
+# Ordering Filter
 class StudentList(ListAPIView):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
-    # filterset_fields = ['city']
-    filterset_fields = ['name','city']
+    filter_backends = [OrderingFilter]
+    # ordering_fields = ['name']
+    ordering_fields = ['name','city']
--- filtering/filtering/settings.py
+++ filtering/filtering/settings.py
@@ -127,5 +127,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
  
 STATIC_URL = 'static/'
 REST_FRAMEWORK = {
-    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
+    # 'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
+    'SEARCH_PARAM': 'q'
+    
 }
